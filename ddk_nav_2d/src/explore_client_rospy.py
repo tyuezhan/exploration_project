b6ceb078404af1535ce133c10b9c230672669582
Yuezhan Tao <yztao@seas.upenn.edu>
2022-02-27 17:17:41 -0500

    Python indent size to 2

--- ddk_nav_2d/src/explore_client_rospy.py
+++ ddk_nav_2d/src/explore_client_rospy.py
@@ -7,17 +7,19 @@ from std_srvs.srv import Trigger, TriggerResponse
 
 client = actionlib.SimpleActionClient("Explore", ExploreAction)
 
+
 def receiveCommand(req):
-    goal = ExploreGoal()
-    client.send_goal(goal)
-    res = TriggerResponse()
-    res.success = True
-    res.message = "Send ExploreGoal to Navigator."
-    return res
+  goal = ExploreGoal()
+  client.send_goal(goal)
+  res = TriggerResponse()
+  res.success = True
+  res.message = "Send ExploreGoal to Navigator."
+  return res
+
 
 if __name__ == "__main__":
-    rospy.init_node('Explore', anonymous=False)
-    cmdServer = rospy.Service("StartExploration", Trigger, receiveCommand)
+  rospy.init_node('Explore', anonymous=False)
+  cmdServer = rospy.Service("StartExploration", Trigger, receiveCommand)
 
-    client.wait_for_server()
-    rospy.spin()
+  client.wait_for_server()
+  rospy.spin()
--- ddk_nav_2d/src/navigator_rospy.py
+++ ddk_nav_2d/src/navigator_rospy.py
@@ -4,8 +4,7 @@ import rospy
 from nav2d_rospy import *
 
 if __name__ == '__main__':
-    rospy.init_node('navigator', anonymous=False)
-    
-    nav2d = Nav2D()
-    rospy.spin()
-    
+  rospy.init_node('navigator', anonymous=False)
+
+  nav2d = Nav2D()
+  rospy.spin()
